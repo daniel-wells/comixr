299ed83ddb3bda26c45e9d61dc33ebf37c9e1517
daniel-wells <daniel-wells@users.noreply.github.com>
2016-09-02 16:56:49 +0100

    rename variables
    b to scale, c to shape, m to mean

--- R/VBGMM.R
+++ R/VBGMM.R
@@ -1,10 +1,9 @@
 
 input.parameters <- data.table(
-  lambda=c(5),
-  m=c(5,2),
+  mean=c(5,2),
   nu=c(2),
-  b=c(4),
-  c=c(0.125),
+  scale=c(4),
+  shape=c(0.125),
   component.type=c(rep("common",1),rep("specific",1)))
 
 #vals.df <- bulkDP[chr==6 & !is.na(segment.no),.(vals=total,seg=segment.no)]
@@ -27,10 +26,14 @@ hist(vals.df$vals,breaks=300,xlim=c(-1,9))
 #'  and second being charachter segment names. Column names can be whatever.
 #'
 #' @param rho.input Numeric value for initial value of rho parameter
-#' @param input.parameters
-#' @param init.max
-#' @param break.parameter Numeric value, when the difference in log likelihood 
-#' between two iterations is less than this value the iteration will stop
+#' 
+#' @param input.parameters Data frame holding initialisation parameters for the prior distribution parameters.
+#' With numeric columns "scale" and "shape" the parameters for the gamma
+#' prior on precision; "mean" and "nu" for the mean and precision parameters for the normal prior on 
+#' the mean; and a charachter column "component.type" with either "common" or "specific"
+#' 
+#' @param max.iterations maximum number of iterations
+#' 
 #' @return A list of parameters
 #'
 #' @examples
@@ -93,37 +96,37 @@ psi <- numeric(length = length(read.count))
 ##### hyperparameters
 # mixing prior
 priors <- vector("list", 5)
-names(priors) <- c("lambda","m","nu","b","c")
+names(priors) <- c("lambda","mean","nu","scale","shape")
 priors$lambda <- 5
 
 # means prior
-priors$m <- mean(read.count)
+priors$mean <- mean(read.count)
 priors$nu <- (sum(abs(range(read.count)))/3)^2
 
 # precisions prior
-priors$b <- 1000
-priors$c <- 0.001
+priors$scale <- 1000
+priors$shape <- 0.001
 
 ##### initialise parameters
 # each specific parameter is a j by k matrix
 specific_parameters <- vector("list", 6)
-names(specific_parameters) <- c("mix_weights","lambda","b","c","nu","m")
+names(specific_parameters) <- c("mix_weights","lambda","scale","shape","nu","mean")
 specific_parameters$mix_weights <- matrix(rep(1/n.specific.components, n.segments * n.specific.components), ncol = n.specific.components)
 specific_parameters$lambda <- matrix(rep(priors$lambda, n.segments * n.specific.components), ncol = n.specific.components)
-specific_parameters$b <- matrix(rep(input.parameters[component.type=="specific"]$b, n.segments), ncol = n.specific.components)
-specific_parameters$c <- matrix(rep(input.parameters[component.type=="specific"]$c, n.segments), ncol = n.specific.components)
+specific_parameters$scale <- matrix(rep(input.parameters[component.type=="specific"]$scale, n.segments), ncol = n.specific.components)
+specific_parameters$shape <- matrix(rep(input.parameters[component.type=="specific"]$shape, n.segments), ncol = n.specific.components)
 specific_parameters$nu <- matrix(rep(input.parameters[component.type=="specific"]$nu, n.segments), ncol = n.specific.components)
-specific_parameters$m <- matrix(rep(input.parameters[component.type=="specific"]$m, each=n.segments), ncol = n.specific.components)
+specific_parameters$mean <- matrix(rep(input.parameters[component.type=="specific"]$mean, each=n.segments), ncol = n.specific.components)
 
 common_parameters <- matrix(nrow = n.common.components, ncol = 6) # vector("list", 6)
-colnames(common_parameters) <- c("mix_weights","lambda","b","c","nu","m")
+colnames(common_parameters) <- c("mix_weights","lambda","scale","shape","nu","mean")
 common_parameters <- as.data.table(common_parameters)
 common_parameters$mix_weights <- rep((1 / n.common.components), n.common.components )
 common_parameters$lambda <- rep(priors$lambda, n.common.components )
-common_parameters$b <- com.param$b
-common_parameters$c <- com.param$c
+common_parameters$scale <- com.param$scale
+common_parameters$shape <- com.param$shape
 common_parameters$nu <- com.param$nu
-common_parameters$m <- com.param$m
+common_parameters$mean <- com.param$mean
 
 
 iter.count <- 0
@@ -138,17 +141,17 @@ for (round in 1:max.iterations){
 # calculate responsibilities for common components
 
 weighting_c = exp(digamma(common_parameters$lambda) - digamma(sum(common_parameters$lambda)))
-beta_tilde_c = exp(digamma(common_parameters$c) + log(common_parameters$b))
+beta_tilde_c = exp(digamma(common_parameters$shape) + log(common_parameters$scale))
 common_topsum <- weighting_c * beta_tilde_c^0.5 *
-  exp(-0.5 * common_parameters$b * common_parameters$c * ( outer(read.count^2, common_parameters$m^2 + common_parameters$nu, FUN="+") - 2 * outer(read.count, common_parameters$m, FUN="*") ) )
+  exp(-0.5 * common_parameters$scale * common_parameters$shape * ( outer(read.count^2, common_parameters$mean^2 + common_parameters$nu, FUN="+") - 2 * outer(read.count, common_parameters$mean, FUN="*") ) )
 
 phi = common_topsum / rowSums(common_topsum)
 
 # calculate responsibilities for specific components
 
 weighting = exp(digamma(specific_parameters$lambda) - digamma(sum(specific_parameters$lambda)))
-beta_tilde = exp(digamma(specific_parameters$c) + log(specific_parameters$b))
-beta_bar = specific_parameters$b * specific_parameters$c
+beta_tilde = exp(digamma(specific_parameters$shape) + log(specific_parameters$scale))
+beta_bar = specific_parameters$scale * specific_parameters$shape
 
 ##### what should rho be?
 rho_weighting = exp(digamma(kappa) - digamma(sum(kappa)))
@@ -158,8 +161,8 @@ for (segment in segment.names){
   
 gamma_topsum[indexes, ] <- weighting[segment, ] * beta_tilde[segment, ]^0.5 *
                             exp(-0.5 * beta_bar[segment, ] * 
-                                  (outer(read.count[indexes]^2, specific_parameters$m[segment, ]^2 + specific_parameters$nu[segment, ], FUN="+") 
-                                    - 2 * outer(read.count[indexes], specific_parameters$m[segment, ], FUN="*")
+                                  (outer(read.count[indexes]^2, specific_parameters$mean[segment, ]^2 + specific_parameters$nu[segment, ], FUN="+") 
+                                    - 2 * outer(read.count[indexes], specific_parameters$mean[segment, ], FUN="*")
                                   )
                                 )
 
@@ -170,18 +173,18 @@ psi_topsum <- (1-rho_weighting[segment]) * rowSums(common_topsum[indexes, , drop
 psi_bottomsum <- psi_topsum + (rho_weighting[segment] * rowSums(gamma_topsum[indexes, , drop=FALSE]))
 psi[indexes] <- psi_topsum / psi_bottomsum
 
-hist(phi[indexes],breaks=100)
-hist(gamma[indexes],breaks=100)
-hist(psi[indexes],breaks=100)
+#hist(phi[indexes],breaks=100)
+#hist(gamma[indexes],breaks=100)
+#hist(psi[indexes],breaks=100)
 }
 
-hist(phi,breaks=100)
-hist(gamma,breaks=100)
-hist(psi,breaks=100)
+#hist(phi,breaks=100)
+#hist(gamma,breaks=100)
+#hist(psi,breaks=100)
 
-sum(is.na(phi))
-sum(is.na(gamma))
-sum(is.na(psi))
+stopifnot(sum(is.na(phi))==0)
+stopifnot(sum(is.na(gamma))==0)
+stopifnot(sum(is.na(psi))==0)
 
 #########################
 #### M Step #############
@@ -206,19 +209,19 @@ for (segment in segment.names){
   specific_parameters$lambda[segment, ] <- N_specific[segment, ] + priors$lambda
   kappa[segment] <- N_rho[segment] + priors$lambda
   
-  sigma2_specific[segment, ] <- y2_weighted_specific[segment, ] + specific_parameters$mix_weights[segment,] * (specific_parameters$m[segment, ]^2 + specific_parameters$nu[segment, ]) - 2 * specific_parameters$m[segment, ] * y_weighted_specific[segment, ]
+  sigma2_specific[segment, ] <- y2_weighted_specific[segment, ] + specific_parameters$mix_weights[segment,] * (specific_parameters$mean[segment, ]^2 + specific_parameters$nu[segment, ]) - 2 * specific_parameters$mean[segment, ] * y_weighted_specific[segment, ]
 
-  specific_parameters$b[segment, ] <- 1/( sigma2_specific[segment, ] * 0.5 * sum.of.1.minus.psi + 1/priors$b)
+  specific_parameters$scale[segment, ] <- 1/( sigma2_specific[segment, ] * 0.5 * sum.of.1.minus.psi + 1/priors$scale)
   
-  specific_parameters$c[segment, ] <- N_specific[segment, ] * 0.5 + priors$c
+  specific_parameters$shape[segment, ] <- N_specific[segment, ] * 0.5 + priors$shape
   
-  t_data_specific[segment, ] <- N_specific[segment, ] * specific_parameters$b[segment, ] * specific_parameters$c[segment, ]
+  t_data_specific[segment, ] <- N_specific[segment, ] * specific_parameters$scale[segment, ] * specific_parameters$shape[segment, ]
   
   m_data_specific[segment, ] <- y_weighted_specific[segment, ] / specific_parameters$mix_weights[segment,]
   
   specific_parameters$nu[segment, ] <- 1/ (1/priors$nu + t_data_specific[segment, ])
   
-  specific_parameters$m[segment, ] <- (1/priors$nu * priors$m) / (1/specific_parameters$nu[segment, ]) + (t_data_specific[segment, ] * m_data_specific[segment, ]) / (1/specific_parameters$nu[segment, ])
+  specific_parameters$mean[segment, ] <- (1/priors$nu * priors$mean) / (1/specific_parameters$nu[segment, ]) + (t_data_specific[segment, ] * m_data_specific[segment, ]) / (1/specific_parameters$nu[segment, ])
 }
 
 # calculate parameters common to all segments
@@ -234,19 +237,19 @@ y2_weighted_common <- colSums( psi * phi * read.count^2 ) / sum.of.psi
 
 common_parameters$lambda <- N_common + priors$lambda
 
-sigma2_common <- y2_weighted_common + common_parameters$mix_weights * (common_parameters$m^2 + common_parameters$nu) - 2 * common_parameters$m * y_weighted_common
+sigma2_common <- y2_weighted_common + common_parameters$mix_weights * (common_parameters$mean^2 + common_parameters$nu) - 2 * common_parameters$mean * y_weighted_common
 
-common_parameters$b <- 1/ (sigma2_common * 0.5 * sum.of.psi + 1/priors$b)
+common_parameters$scale <- 1/ (sigma2_common * 0.5 * sum.of.psi + 1/priors$scale)
 
-common_parameters$c <- N_common * 0.5 + priors$c
+common_parameters$shape <- N_common * 0.5 + priors$shape
 
-t_data <- N_common * (common_parameters$b * common_parameters$c)
+t_data <- N_common * (common_parameters$scale * common_parameters$shape)
 
 m_data <- y_weighted_common / common_parameters$mix_weights
 
 common_parameters$nu <- 1/(1/priors$nu + t_data)
 
-common_parameters$m <- (1/priors$nu * priors$m) / (1/common_parameters$nu) + (t_data * m_data) / (1/common_parameters$nu)
+common_parameters$mean <- (1/priors$nu * priors$mean) / (1/common_parameters$nu) + (t_data * m_data) / (1/common_parameters$nu)
 
 iter.count <- iter.count + 1
 print(paste("Iteration:",iter.count))
@@ -255,7 +258,15 @@ print(paste("Iteration:",iter.count))
 
 # parse output
 
-output <- list(common_parameters=common_parameters,specific_parameters=specific_parameters,rho=rho,kappa=kappa)
+common_parameters$variance = 1/(common_parameters$scale * common_parameters$shape)
+specific_parameters$variance = 1/(specific_parameters$scale * specific_parameters$shape)
+
+output <- list(
+  common_parameters = common_parameters,
+  specific_parameters = specific_parameters,
+  rho = rho,
+  kappa = kappa,
+  iteration = iter.count)
 
 return(output)
 
